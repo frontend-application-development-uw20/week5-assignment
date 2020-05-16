766166ff06753efa81b10d81424460342724c1cf
chanjessica <jchan_sea@yahoo.com>
2020-05-16 16:33:52 -0700

    fix title

--- calling-api/src/covid19country.js
+++ calling-api/src/covid19country.js
@@ -3,29 +3,44 @@ import React from 'react';
 export default class Covid19country extends React.Component {
     state = {
         detail: [],
-        CountryName: ''
+        CountryName: '',
+        Country: '',
+        loading: true,
+        error: false
     }
 
     componentDidMount() {
-        console.log(this.props);
+        // console.log(this.props);
         const { CountryName } = this.props.match.params;
         this.setState({ CountryName: CountryName })
-        console.log(CountryName);
-        // console.log(CountryName.replace(/\s+/g, '-').toLowerCase());
 
-        // const code = CountryName.replace(/\s+/g, '-').toLowerCase();
-        // console.log(code);
+        const code = CountryName.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+        this.setState({ Country: code })
 
-        // fetch(`https://api.covid19api.com/live/country/${CountryName}`)
+        // fetch(`https://api.covid19api.com/live/country/${CountryName}/status/confirmed`)
         fetch(`https://api.covid19api.com/total/country/${CountryName}`)
             .then(res => res.json())
-            .then(data => this.setState({ detail: data }));
+            .then(data => {
+                console.log(data.Error);
+                this.setState({ detail: data, loading: false, error: false })
+            }
+            )
+            .catch(err => this.setState({ loading: false, error: true }))
     }
 
     render() {
+        if (this.state.loading) {
+            return <div>....loading</div>;
+        }
+
+        if (this.state.error) {
+            return <div> Try to reload again! </div>;
+        }
         return (
             <>
-                <h2> {this.state.CountryName}  </h2>
+                <h2> ***   {this.state.Country} ***
+                    {/* <img src="https://www.countryflags.io/{country_code}/flat/64.png"> */}
+                </h2>
                 <div className="detail">
                     {this.state.detail.map((c, idx) => <div key={idx}>
                         <b>{c.Date.substring(0, 10)} Total={parseInt(c.Confirmed).toLocaleString()} </b>
